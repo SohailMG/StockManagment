ac1c8d43921b6f977efc94317dc320a89d3f35ec
Sohail Gsais <gsais.sohail9@gmail.com>
2021-01-02 14:04:59 +0000

    added function to read custom data form file

--- sellProduct.cpp
+++ sellProduct.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <fstream>
 #include "header.h"
 
 
@@ -10,8 +12,42 @@ class SellProduct : public Product
 {
 
 public:
+    string line;
+    string tempString;
+
+    void readInventory(){
+        ifstream datafile("Inventory.txt");
+        if (!datafile.is_open())
+        {
+            cout << "File failed to open "<<endl;
+        
+        }
+
+
+        while (getline(datafile, line))
+        {
+            stringstream ss(line);
+
+            getline(ss, tempString,':');
+            itemCode = stoi(tempString);
+            getline(ss, itemName,':');
+            getline(ss, itemType,':');
+            getline(ss, tempString,':');
+            itemPrice = stoi(tempString);
+
+            cout << itemCode << itemType << itemName << itemPrice << endl;
+        }
+        
+        
+
+
+
+        
+    }
+
     int sellProduct(int ID)
     {
+
 
         cout << " Item Sold : " << ID << endl;
     }
