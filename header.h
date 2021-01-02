e51536040cfbbf65c5d2b83c13966f841dce4839
Sohail Gsais <sohailgsais@sohails-mbp.home>
2021-01-02 11:47:19 +0000

    added txt file to store and load data from, and header file

---
+++ header.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+int getProductID(int itemCode);
+
+int showMenue();
+
+void currentMenu(int menueOption);
+
+int sellProduct(int ID);
+
+void product_info();
+
+void show_added();
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <header.h>
 
 using namespace std;
 
@@ -30,22 +31,25 @@ public:
         cout << " Sell Item--------------- >1" << endl;
         cout << " Add new item------------ >2" << endl;
         cout << " Restock Product--------- >3" << endl;
-        cout << " View Reports------------ >4" << endl;
+        cout << " Update Stock------------ >4" << endl;
+        cout << " View Reports------------ >5" << endl;
         cout << " Choose an Option > ";
         cin >> menu;
 
         return menu;
     }
 };
+
 class Menue:public Product
 {
+    
     public:
         string ActiveMenue;
-        string MENUE     = "----Main Menue----";
-        string SELL      = "----Sell Item----";
-        string ADD       = "----Add Item----";
-        string RESTOCK   = "----Restock Item----";
-        string UPDATE    = "----Update Item----";
+        string MENUE     = "-------Main Menue--------";
+        string SELL      = "-------Sell Item---------";
+        string ADD       = "--------Add Item---------";
+        string RESTOCK   = "------Restock Item-------";
+        string UPDATE    = "-------Update Item-------";
         string VIEW      = "----View Sales Report----";
 
         void currentMenu(int menueOption){
@@ -59,6 +63,12 @@ class Menue:public Product
                 ActiveMenue = ADD;
                 break;
             case 3:
+                ActiveMenue = RESTOCK;
+                break;
+            case 4:
+                ActiveMenue = UPDATE;
+                break;
+            case 5:
                 ActiveMenue = VIEW;
                 break;
             
@@ -83,19 +93,76 @@ public:
     }
 };
 
+class AddProduct : public Product
+{
+    public:
+    void product_info()
+    {
+
+        cout << " Enter Item Code     : " << endl;
+        cin >> itemCode;
+        cout << " Enter Item Name     : " << endl;
+        cin >> itemName;
+        cout << " Enter Item Type     : " << endl;
+        cin >> itemType;
+        cout << " Enter Item Price    : " << endl;
+        cin >> itemPrice;
+        cout << " Enter Item Quantity : " << endl;
+        cin >> quantity;
+        
+    }
+
+    void show_added(){
+
+        cout << "New Product added" << endl;
+        cout << "ID       : "<< itemCode << endl;
+        cout << "Name     : "<< itemName << endl;
+        cout << "Type     : "<< itemType << endl;
+        cout << "Price    : "<< itemPrice << endl;
+        cout << "quantity : "<< quantity << endl;
+
+    }
+    
+    
+
+};
+
 int main(void)
 {
 
     SellProduct p;
     Product P;
     Menue M;
+    AddProduct A;
 
 
     int menueOption = P.showMenu();
     cout << "\n\n"<<endl;
-    M.currentMenu(menueOption);
-    int ID = p.getProductID(p.itemCode);
-    p.sellProduct(ID);
+    
+
+    switch (menueOption)
+    {
+    case 1:
+    {
+        M.currentMenu(menueOption);
+        int ID = p.getProductID(p.itemCode);
+        p.sellProduct(ID);
+        break;
+    }
+
+    {
+    case 2:
+        M.currentMenu(menueOption);
+        A.product_info();
+        A.show_added();
+        break;
+    }
+    default:
+        break;
+    }
+
+
+    
 
     return 0;
 }
