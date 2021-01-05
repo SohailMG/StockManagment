1e8eec61c146f615b4e2941744c72aae4e16f4f8
Sohail Gsais <gsais.sohail9@gmail.com>
2021-01-05 19:58:25 +0000

    added Restock functionality

---
+++ RestockProduct.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include "header.h"
+
+class RestockProduct : public Product
+{
+
+public:
+    string line;
+    string tempString;
+    ofstream outfile;
+    ifstream temp;
+    bool deleted_old;
+    int restock_amount;
+
+    void readInventory()
+    {
+        ofstream tempFile("temp.txt");
+        ifstream datafile("Inventory.txt");
+        if (!datafile.is_open())
+        {
+            cout << "File failed to open " << endl;
+        }
+
+        string id = to_string(getProductID(itemCode));
+        cout << "Restock Amount > ";
+        cin >> restock_amount;
+
+        while (getline(datafile, line))
+        {
+            if (line == "")
+                continue;
+
+            if (id == line.substr(0, 4))
+            {
+                stringstream ss(line);
+                getline(ss, tempString, ':');
+                itemCode = stoi(tempString);
+                getline(ss, itemName, ':');
+                getline(ss, itemType, ':');
+                getline(ss, tempString, ':');
+                itemPrice = stoi(tempString);
+                getline(ss, tempString, ':');
+                quantity = stoi(tempString) + restock_amount;
+
+                tempFile
+                    << itemCode << ':'
+                    << itemName << ':'
+                    << itemType << ':'
+                    << itemPrice << ':'
+                    << quantity << endl;
+            }
+            if (id != line.substr(0, 4))
+            {
+                stringstream ss(line);
+                getline(ss, tempString, ':');
+                itemCode = stoi(tempString);
+                getline(ss, itemName, ':');
+                getline(ss, itemType, ':');
+                getline(ss, tempString, ':');
+                itemPrice = stoi(tempString);
+                getline(ss, tempString, ':');
+                quantity = stoi(tempString);
+
+                tempFile
+                    << itemCode << ':'
+                    << itemName << ':'
+                    << itemType << ':'
+                    << itemPrice << ':'
+                    << quantity << endl;
+            }
+        }
+        tempFile.close();
+    }
+    void delete_old()
+    {
+
+        outfile.open("Inventory.txt", std::ios_base::app);
+        if (remove("Inventory.txt"))
+            deleted_old = true;
+        else
+            deleted_old = false;
+    }
+    void update_inventory()
+    {
+        ifstream temp("temp.txt");
+        string lines;
+        while (getline(temp, lines))
+        {
+            ofstream outfile("Inventory.txt",std::ios_base::app);
+            outfile << lines;
+            outfile << '\n';
+
+        }
+    }
+};
